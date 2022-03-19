88c61d02a07bf45cf8c78ad5cab538e7106e9b40
P. Peter <h163007@stud.u-szeged.hu>
2022-03-19 22:07:35 +0100

    Run Black code formatter.

--- app.py
+++ app.py
@@ -4,29 +4,31 @@ from engineio.payload import Payload
 
 
 app = Flask(__name__)
-#Payload.max_decode_packets = 999999
+# Payload.max_decode_packets = 999999
 socketio = SocketIO(app)
+
 
 @app.route("/")
 def index():
-    return render_template('index.html')
+    return render_template("index.html")
+
 
 @app.route("/nezo")
 def nezo():
-    return render_template('nezo.html')
+    return render_template("nezo.html")
+
 
 @app.route("/rajzolo")
 def rajzolo():
-    return render_template('rajzolo.html')
+    return render_template("rajzolo.html")
 
-@socketio.on('message')
+
+@socketio.on("message")
 def print_message(message):
     print(message)
     socketio.emit("message", message)
 
-@socketio.on('drawing')
+
+@socketio.on("drawing")
 def drawing(message):
-    socketio.emit('drawing', message)
-
-
-
+    socketio.emit("drawing", message)
