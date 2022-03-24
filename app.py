ae6bba2796f3e2740eed2b644b7f3b40dd7f0d69
h985712 <h985712@stud.u-szeged.hu>
2022-03-24 22:58:57 +0100

    chat name, rounds, game_over, new_game feature added #6,#5,#3

--- app.py
+++ app.py
@@ -1,12 +1,15 @@
+import imp
 from flask import Flask, render_template
 from flask_socketio import SocketIO
+from engineio.payload import Payload
 import random
+from eventlet import wsgi
+import eventlet
 
 
 app = Flask(__name__)
 # Payload.max_decode_packets = 999999
 socketio = SocketIO(app)
-
 
 @app.route("/")
 def index():
@@ -21,18 +24,6 @@ def nezo():
 @app.route("/rajzolo")
 def rajzolo():
     return render_template("rajzolo.html")
-
-
-@socketio.on("message")
-def print_message(message):
-    print(message)
-    socketio.emit("message", message)
-
-
-@socketio.on("drawing")
-def drawing(message):
-    socketio.emit("drawing", message)
-
 
 def give_random_word():
     dictionary = [
@@ -58,3 +49,55 @@ def give_random_word():
         "glasses",
     ]
     return random.choice(dictionary)
+
+
+@socketio.on("give_random_word")
+def random_word(rnd_word=""):
+    global global_word
+    global_word=give_random_word()
+    #print(f"fgv belül: {global_word}")
+    socketio.emit("give_random_word", global_word)
+
+@socketio.on("message")
+def print_message(message):
+    print(message)
+    msg_index = int(message.index(':'))
+    msg_len = int(len(message))
+    msg_word = message[msg_index+2:msg_len]
+    win_name = message[0:msg_index]
+    winner_name(win_name)
+    #print(f"Global_word:{global_word}, Msg_word:{msg_word}.")
+    if str(msg_word) == str(global_word):
+        print("equal")
+        game_end()
+    socketio.emit("message", message)
+
+
+@socketio.on("drawing")
+def drawing(message):
+    socketio.emit("drawing", message)
+
+@socketio.on("game_end")
+def game_end():
+    print("Game over")
+    socketio.emit("game_end")
+
+@socketio.on("winner_name")
+def winner_name(name):
+    socketio.emit("winner_name", name)
+
+@socketio.on("new_game")
+def new_game():
+    socketio.emit("new_game")
+
+@socketio.on("viewer_joinned")
+def viewer_joinned():
+    print("viewer_joinned")
+    #random_word()
+    socketio.emit("viewer_joinned")
+
+if __name__ == '__main__':
+  app.debug=True
+  wsgi.server(eventlet.listen(('',8080)), app)
+
+
